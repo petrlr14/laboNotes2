ea2b745142f965beef4f7416a204b19488cabfb0
petrlr14 <00057616@uca.edu.sv>
2018-05-27 10:02:20 -0600

    drawer items linked with fragments

--- app/src/main/java/com/pdm00057616/labonotesver2/activities/MainActivity.java
+++ app/src/main/java/com/pdm00057616/labonotesver2/activities/MainActivity.java
@@ -1,7 +1,9 @@
 package com.pdm00057616.labonotesver2.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +11,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 import com.pdm00057616.labonotesver2.R;
+import com.pdm00057616.labonotesver2.fragments.AddNoteFragment;
+import com.pdm00057616.labonotesver2.fragments.NotesFragment;
+import com.pdm00057616.labonotesver2.fragments.ProfileFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,9 +46,38 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_dehaze_black_24dp);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         navigationView.setNavigationItemSelectedListener((item) -> {
+                    Fragment fragment;
+                    int type = -1;
+                    switch (item.getItemId()) {
+                        case R.id.account_section:
+                            type = 0;
+                            break;
+                        case R.id.notes_section:
+                            type = 1;
+                            break;
+                        case R.id.add_note_section:
+                            type=2;
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().
+                            replace(R.id.main_content, setFragment(type)).
+                            commit();
+                    drawerLayout.closeDrawers();
                     return true;
                 }
         );
 
+    }
+
+    private Fragment setFragment(int type) {
+        switch (type) {
+            case 0:
+                return new ProfileFragment();
+            case 1:
+                return new NotesFragment();
+            case 2:
+                return new AddNoteFragment();
+        }
+        return null;
     }
 }
