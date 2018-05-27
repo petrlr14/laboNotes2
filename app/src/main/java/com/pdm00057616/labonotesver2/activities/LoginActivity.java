38b7741326034abb80c17eea723445e97f02358e
petrlr14 <00057616@uca.edu.sv>
2018-05-27 16:02:02 -0600

    added login logic

--- app/src/main/java/com/pdm00057616/labonotesver2/activities/LoginActivity.java
+++ app/src/main/java/com/pdm00057616/labonotesver2/activities/LoginActivity.java
@@ -3,7 +3,6 @@ package com.pdm00057616.labonotesver2.activities;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -11,16 +10,18 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.pdm00057616.labonotesver2.R;
-import com.pdm00057616.labonotesver2.database.DB;
-import com.pdm00057616.labonotesver2.database.daos.UserDao;
 import com.pdm00057616.labonotesver2.database.repositories.UserRepository;
 import com.pdm00057616.labonotesver2.models.User;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LoginActivity extends AppCompatActivity {
 
     private Button button;
     private EditText editTextUsername;
     private UserRepository userRepository;
+    private List<User> users;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +35,7 @@ public class LoginActivity extends AppCompatActivity {
         editTextUsername=findViewById(R.id.edit_text_username);
         button=findViewById(R.id.button_enter);
         button.setOnClickListener(v->setButtonAction());
+        userRepository.getAllUsers().observe(this, usersList->users=usersList);
     }
 
     private void setButtonAction(){
@@ -48,9 +50,15 @@ public class LoginActivity extends AppCompatActivity {
 
     private void log(){
         String username=editTextUsername.getText().toString();
-        User user=userRepository.getUserByUsername(editTextUsername.getText().toString());
-        if(user==null){
+        List<String>usernames=new ArrayList<>();
+        if(users!=null){
+            for(User x:users){
+                usernames.add(x.getUsername());
+            }
+        }
+        if(!usernames.contains(username)){
             userRepository.insert(new User(username, 0));
+            Toast.makeText(this, "User created", Toast.LENGTH_SHORT).show();
         }
         createSP();
         Intent intent = new Intent(this, MainActivity.class);
@@ -59,9 +67,9 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void createSP(){
-        SharedPreferences preferences=this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences preferences=this.getSharedPreferences("log",Context.MODE_PRIVATE);
         SharedPreferences.Editor editor=preferences.edit();
         editor.putString(getString(R.string.login_token), editTextUsername.getText().toString());
-        editor.apply();
+        editor.commit();
     }
 }
