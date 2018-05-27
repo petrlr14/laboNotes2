86f8d90ad01f50f9550bc79d22b6c02fbc4af6cc
petrlr14 <00057616@uca.edu.sv>
2018-05-27 15:54:35 -0600

    added user by username asynctask

--- app/src/main/java/com/pdm00057616/labonotesver2/database/repositories/UserRepository.java
+++ app/src/main/java/com/pdm00057616/labonotesver2/database/repositories/UserRepository.java
@@ -3,6 +3,8 @@ package com.pdm00057616.labonotesver2.database.repositories;
 import android.app.Application;
 import android.arch.lifecycle.LiveData;
 import android.os.AsyncTask;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import com.pdm00057616.labonotesver2.database.DB;
 import com.pdm00057616.labonotesver2.database.daos.UserDao;
@@ -14,6 +16,7 @@ public class UserRepository {
 
     private UserDao userDao;
     private LiveData<List<User>> allUsers;
+    static User user;
 
     public UserRepository(Application application) {
         DB db=DB.getInstance(application);
@@ -29,13 +32,21 @@ public class UserRepository {
         new insertAsyncTask(userDao).execute(user);
     }
 
+    public static void setUser(User aux){
+        user=aux;
+    }
 
     public LiveData<User> getUser() {
         return userDao.getUser();
     }
 
     public User getUserByUsername(String username){
-        return userDao.getUserByUsername(username);
+        new getUsernameAsyncTask(userDao).execute(username);
+        return user;
+    }
+
+    public void nukeTable(){
+        new nukeTableAsyncTask(userDao).execute();
     }
 
     private static class insertAsyncTask extends AsyncTask<User, Void, Void>{
@@ -49,6 +60,40 @@ public class UserRepository {
         @Override
         protected Void doInBackground(User... users) {
             mUserDao.insert(users[0]);
+            return null;
+        }
+    }
+
+    private static class getUsernameAsyncTask extends AsyncTask<String, Void, User>{
+
+        private UserDao userDao;
+
+        private getUsernameAsyncTask(UserDao userDao) {
+            this.userDao = userDao;
+        }
+
+        @Override
+        protected User doInBackground(String... strings) {
+            return userDao.getUserByUsername(strings[0]);
+        }
+
+        @Override
+        protected void onPostExecute(User user) {
+            super.onPostExecute(user);
+            UserRepository.setUser(user);
+        }
+    }
+
+    private static class nukeTableAsyncTask extends AsyncTask<Void, Void, Void>{
+        private UserDao userDao;
+
+        private nukeTableAsyncTask(UserDao userDao) {
+            this.userDao = userDao;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            userDao.nuke();
             return null;
         }
     }
