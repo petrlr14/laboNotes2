f0ce3732ce0b6394f32ac7b2e2148fa6b169ec3e
petrlr14 <00057616@uca.edu.sv>
2018-05-27 10:03:14 -0600

    added foreignkey

--- app/src/main/java/com/pdm00057616/labonotesver2/models/Note.java
+++ app/src/main/java/com/pdm00057616/labonotesver2/models/Note.java
@@ -8,10 +8,18 @@ import android.arch.persistence.room.PrimaryKey;
 
 import static android.arch.persistence.room.ForeignKey.CASCADE;
 
-@Entity(indices = {@Index("user_id")},foreignKeys = @ForeignKey(entity = User.class,
-        parentColumns = "username",
-        childColumns = "user_id",
-        onDelete = CASCADE))
+@Entity(indices = {@Index("user_id")},
+        foreignKeys = {
+                @ForeignKey(entity = User.class,
+                        parentColumns = "username",
+                        childColumns = "user_id",
+                        onDelete = CASCADE),
+                @ForeignKey(entity = Category.class,
+                        parentColumns = "id",
+                        childColumns = "category_id",
+                        onDelete = CASCADE)
+        }
+)
 public class Note {
 
     @PrimaryKey(autoGenerate = true)
@@ -27,10 +35,14 @@ public class Note {
     @ColumnInfo(name = "user_id")
     private String username;
 
-    public Note(String text, String title, String username) {
+    @ColumnInfo(name = "category_id")
+    private int categoryID;
+
+    public Note(String text, String title, String username, int categoryID) {
         this.text = text;
         this.title = title;
         this.username = username;
+        this.categoryID = categoryID;
     }
 
     public int getId() {
@@ -51,5 +63,9 @@ public class Note {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public int getCategoryID() {
+        return categoryID;
     }
 }
