b7279cb5bf1f96f1b634c483d17228f6d6f25f0b
petrlr14 <00057616@uca.edu.sv>
2018-05-27 15:53:22 -0600

    added toast message

--- app/src/main/java/com/pdm00057616/labonotesver2/fragments/AddCategoryFragment.java
+++ app/src/main/java/com/pdm00057616/labonotesver2/fragments/AddCategoryFragment.java
@@ -1,6 +1,9 @@
 package com.pdm00057616.labonotesver2.fragments;
 
+import android.app.Activity;
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -10,19 +13,24 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.pdm00057616.labonotesver2.R;
 import com.pdm00057616.labonotesver2.models.Category;
+import com.pdm00057616.labonotesver2.viewmodels.CategoryViewModel;
 
 public class AddCategoryFragment extends Fragment {
 
     private Context context;
+    private Activity activity;
     private EditText editTextCategoryName;
     private Button button;
+    private CategoryViewModel categoryViewModel;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        activity=getActivity();
     }
 
     @Nullable
@@ -30,6 +38,7 @@ public class AddCategoryFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.add_category_layout, container, false);
         editTextCategoryName=view.findViewById(R.id.text_view_category_name);
+        categoryViewModel= ViewModelProviders.of(this).get(CategoryViewModel.class);
         button=view.findViewById(R.id.button_done);
         button.setOnClickListener((v)->setButtonAction());
         return view;
@@ -38,8 +47,16 @@ public class AddCategoryFragment extends Fragment {
     private void setButtonAction(){
         String name=editTextCategoryName.getText().toString();
         if(!name.equals("")){
-            Category category=new Category(name, "");
+            categoryViewModel.insert(new Category(name, getUser()));
             editTextCategoryName.setText("");
+            Toast.makeText(context, "Category added", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(context, "Field can't be empty", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private String getUser(){
+        SharedPreferences preferences=activity.getSharedPreferences("log", Context.MODE_PRIVATE);
+        return preferences.getString(getString(R.string.login_token), "");
     }
 }
